51d371cb8bc8b2aa1da4995b9153f2abc7ced182
aungtuntun-9823 <e1509823@u.nus.edu>
2026-04-24 21:17:33 +0800

    Fixed sonarQube feedback.

--- src/main/java/com/retailpulse/infrastructure/config/FeignConfig.java
+++ src/main/java/com/retailpulse/infrastructure/config/FeignConfig.java
@@ -21,7 +21,7 @@ public class FeignConfig {
 
     private final Tracer tracer;
 
-    public FeignConfig(@Nullable Tracer tracer) {
+    public FeignConfig(Tracer tracer) {
         this.tracer = tracer;
     }
 
@@ -35,8 +35,8 @@ public class FeignConfig {
         return template -> {
 
             if (tracer != null && tracer.currentSpan() != null) {
-                template.header("X-B3-TraceId", tracer.currentSpan().context().traceId());
-                template.header("X-B3-SpanId", tracer.currentSpan().context().spanId());
+                template.header("X-B3-TraceId", Objects.requireNonNull(tracer.currentSpan()).context().traceId());
+                template.header("X-B3-SpanId", Objects.requireNonNull(tracer.currentSpan()).context().spanId());
             }
 
             String token = extractBearerToken();
